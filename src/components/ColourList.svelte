3452f00907e4eab4fdf908eb167e706b7602d215
Antony Jones <antony@beyonk.com>
2020-06-16 09:42:56 +0100

    intial commit

---
+++ src/components/ColourList.svelte
@@ -0,0 +1,15 @@
+<script>
+  const colours = {
+    red: 'ff0000',
+    blue: '0000ff',
+    green: '00ff00'
+  }
+</script>
+
+<ul>
+  {#each Object.entries(colours) as [ colour, hex ]}
+  <li>
+    <a href="/colour/{hex}" rel="prefetch">See {colour}</a>
+  </li>
+  {/each}
+</ul>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Fun with colours</h1>
+
+<ColourList />
+
+<script>
+	import ColourList from '../components/ColourList.svelte'
+</script>
