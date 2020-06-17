1f15978e13d67eac87a3dcdc0abf1ad89d47316e
Antony Jones <antony@beyonk.com>
2020-06-17 12:15:29 +0100

    add store to demo

---
+++ src/components/ColourBlock.svelte
@@ -0,0 +1,17 @@
+<div style="background-color: #{$currentColour};">
+</div>
+
+<script>
+  import { readable } from 'svelte/store'
+
+  export let hex
+
+  const currentColour = readable(hex)
+</script>
+
+<style>
+	div {
+		height: 100px;
+		width: 100px;
+	}
+</style>
